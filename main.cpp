0f15c8e96aaae22d1fafc1bde62073fee628dca4
JauniKappauni <social.jauni@gmail.com>
2025-12-15 15:51:29 +0100

    refactor: reset logic and clean codebase

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
 #include "string.h"
-#include <iostream>
 int main()
 {
-    // test of strlen()
-    /*
-    string str;
-    char str1[] = "Hello World";
-    char str2[] = "";
-    char str3[] = "This is a test";
-    std::cout << str.strlen(str1) << std::endl;
-    std::cout << str.strlen(str2) << std::endl;
-    std::cout << str.strlen(str3) << std::endl;
-    */
 }
--- string.h
+++ string.h
@@ -1,24 +1,16 @@
 class string
 {
 private:
-    char *data = nullptr;
-
+    
 public:
-    string()
-    {
-        data = new char[1];
-        data[0] = '\0';
-    };
-    ~string()
-    {
-        delete[] data;
-    };
-
-    int strlen(char *str)
-    {
-        unsigned int i = 0;
-        for (i; str[i] != '\0'; i++)
-            ;
-        return i;
-    }
+    string();
+    ~string();
 };
+
+string::string()
+{
+}
+
+string::~string()
+{
+}
